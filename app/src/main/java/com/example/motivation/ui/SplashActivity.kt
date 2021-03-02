965a7d8fedcf97c770aa97740a8a3fdfb706bf3a
Douglas vinicius <douglasv.defreitas@gmail.com>
2021-03-02 15:58:43 -0300

    feat - Implementação do SecurityPreferences - Organizão dos arquivos em pastas expecificas

--- app/src/main/java/com/example/motivation/ui/SplashActivity.kt
+++ app/src/main/java/com/example/motivation/ui/SplashActivity.kt
@@ -1,24 +1,32 @@
-package com.example.motivation
+package com.example.motivation.ui
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
 import android.view.View
 import android.widget.Toast
+import com.example.motivation.R
+import com.example.motivation.infra.SecurityPreferences
 import kotlinx.android.synthetic.main.activity_splash.*
 
  class SplashActivity() : AppCompatActivity(), View.OnClickListener {
 
+     private lateinit var mSecurityPreferences: SecurityPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        mSecurityPreferences = SecurityPreferences(this)
+
         if (supportActionBar !=null){
             supportActionBar!!.hide() 
         }
         buttonSave.setOnClickListener(this)
+
+        val shared = this.getSharedPreferences("motivation", Context.MODE_PRIVATE)
+        shared.edit().putString("key","value").apply()
     }
 
     override fun onClick(view: View) {
