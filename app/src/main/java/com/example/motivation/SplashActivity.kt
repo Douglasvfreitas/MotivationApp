077def23e78e4f30e53df45753a1edb0ff66525b
Douglas vinicius <douglasv.defreitas@gmail.com>
2021-03-02 11:09:41 -0300

    feat - foi adicionado eventos de click na splashActivity - aviso de erro para campo de nome em branco ou telcas especiais  - navegação entre telas

--- app/src/main/java/com/example/motivation/SplashActivity.kt
+++ app/src/main/java/com/example/motivation/SplashActivity.kt
@@ -1,9 +1,16 @@
 package com.example.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_splash.*
 
-class SplashActivity : AppCompatActivity() {
+ class SplashActivity() : AppCompatActivity(), View.OnClickListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -11,5 +18,24 @@ class SplashActivity : AppCompatActivity() {
         if (supportActionBar !=null){
             supportActionBar!!.hide() 
         }
+        buttonSave.setOnClickListener(this)
     }
+
+    override fun onClick(view: View) {
+       val id = view.id
+        if (id == R.id.buttonSave){
+            handleSave()
+        }
+
+    }
+    private fun handleSave(){
+        val name = editName.text.toString()
+        if (name != ""){
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            Toast.makeText(this, "Informe seu nome!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
+
